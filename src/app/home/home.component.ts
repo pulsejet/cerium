0ee1de88de50ca492aa953366167bdcc69aeeedc
e-aakash <09e.aakash@gmail.com>
2019-07-11 09:46:20 +0530

    Add component details for viewing all created forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
+interface Forms {
+  ids: string[]
+}
 
 @Component({
   selector: 'app-home',
@@ -7,13 +12,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  forms: Forms;
 
   constructor(
     public router: Router,
+    public http: HttpClient,
   ) { }
 
   ngOnInit() {
-    this.router.navigate(['/new']);
+    const Observable = this.http.get<Forms>(`/api/forms`).subscribe(f => {
+      if (f.ids.length === 0) {
+        this.router.navigate(['/new']);
+      } else {
+        this.forms = f;
+      }
+    })
   }
 
 }
